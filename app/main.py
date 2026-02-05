c7e43bd50ab0e9c049c72a3bd762496d8e76c7d3
ManavPatel86 <202301158@dau.ac.in>
2026-02-06 00:34:38 +0530

    Updated UI

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-
+from fastapi.staticfiles import StaticFiles
 from app.ml.inference import predict_spam
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
 
 @app.get("/", response_class=HTMLResponse)
